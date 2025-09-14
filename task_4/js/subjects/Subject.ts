263b72b4cb383e089d2622cc10b516d2d5260643
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-14 14:57:08 +0300

    task_4 relocation

---
+++ task_4/js/subjects/Subject.ts
@@ -0,0 +1,12 @@
+// task_4/js/subjects/Subject.ts
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    teacher: Teacher;
+
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
